57b4f1ecfcee99c97d36e6ee6e89304e34b2ac06
psoflam <91235526+psoflam@users.noreply.github.com>
2022-09-04 09:45:10 -0400

    Login 80%done.(Needs Renamed) Need connected to back end
    Added Log in box. Started having issues with slider cards when starting program in browser
    
    rename firstname = User Name
    rename lastname = Password
    change input type?

--- TravelApp-Front-end-colin-main/src/components/Header/Login/Login.js
+++ TravelApp-Front-end-colin-main/src/components/Header/Login/Login.js
@@ -2,5 +2,30 @@ import React from "react";
 import "./login.css";
 
 export default function Login() {
-  return <div id="login">Login</div>;
+  return <div id="login">
+    
+      <form class="form" id="frm1" action="/action_page.php">
+      First name: <input type="text" name="fname" placeholder="First Name"></input><br></br>
+      Last name: <input type="text" name="lname" placeholder="Last Name"></input><br></br>
+      <input type="button" onClick="myFunction()" value="Submit">Log In</input>
+      </form>
+
+{/* <script>
+function myFunction() 
+{
+  document.getElementById("frm1").submit()
+}
+</script> */}
+
+
+    Login
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>;
 }
