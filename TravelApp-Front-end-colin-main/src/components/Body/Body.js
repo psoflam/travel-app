0dda06d6e444a315947996cea85425f3fc7f09ec
psoflam <91235526+psoflam@users.noreply.github.com>
2022-09-05 15:54:22 -0400

    Body & fonts Done
    Finished CSS for body. Finished header and slider , added fonts into index.html

--- TravelApp-Front-end-colin-main/src/components/Body/Body.js
+++ TravelApp-Front-end-colin-main/src/components/Body/Body.js
@@ -4,38 +4,42 @@ import "./body.css";
 export default function Body() {
   return (
     <div id="body">
-      <div className="div1">
-        <p className="p1">
-          <img
+      <div className="div1"><img
             className="image1"
             src="https://images.unsplash.com/photo-1522199710521-72d69614c702?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
             alt=""
           />
-          Looking for that weekend Getaway?
+        <p >
+          
+          Looking for that weekend Getaway?, To travel from one place to another for different purposes is generally called travelling. People do not travel always for the same purposes. Someone travels to gain knowledge and experience, someone travels for pleasure, someone travels for business purposes etc. It has much educative value. It is a part of education. Our education and book knowledge remain imperfect without travelling.
         </p>
       </div>
-
-      <div className="div2">
-        <p className="p2">
-          <span className="textTwo">Need time away from the kids?</span>
-          <img
+<hr></hr>
+      <div className="div2"><img
             className="image2"
             src="https://images.unsplash.com/photo-1513807016779-d51c0c026263?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt=""
           />
-        </p>
-      </div>
+        <p >
+          Need time away from the kids? There is something uniquely refreshing about simply spending time being. Not being a Spouse, Child or Parent. Not even a friend or co-worker. Just a woman without an agenda or to-do list in hand. In being, I found compassion for myself instead of the usual pressure to do more, more, more.
 
-      <div className="div3">
-        <p className="p3">
-          <img
+I don’t think you need to be a 45-year old mother or father of two to understand. The time and space to be alone with oneself — with no expectations of accomplishment or progress — is something we all need more of. Yet, it’s not something our society encourages us to pursue.
+          </p> 
+       
+      </div>
+<hr></hr>
+      <div className="div3"><img
             className="image3"
             src="https://images.unsplash.com/photo-1529951185765-5e58e46d79e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt=""
           />
-          <span className="textThree">Or Just looking to explore?</span>
+        <p >
+          
+          Or Just looking to explore? No matter who you are, or where you are going, our travel brands help every type of traveler not only find the trip that’s right for them, but get the best value every time.
+          Man is a traveler by nature. He does not like to keep static at a place for a long time. So, people like to visit new places whenever they find time and opportunity. People travel for different purposes. Some travel for getting an education and some travel for pleasure. Many people also travel for business and employment. Travelling is, indeed, very pleasant. It is useful too. By traveling to new places we can know new people and learn many new things.
         </p>
       </div>
+      <hr></hr>
     </div>
   );
 }
